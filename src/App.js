bdb3394ac8060570a48f2396a247a565edf143d6
Linda Lei <linda@markedly.com>
2020-07-21 20:30:12 -0700

    added new component for ChatBubble and removed unused code. Also built the data object for rendering rooms/users/messages info all after the chat app is mounted

--- src/App.js
+++ src/App.js
@@ -2,6 +2,7 @@ import React from 'react';
 import axios from 'axios';
 import faker from 'faker';
 import './App.css';
+import ChatBubble from './components/ChatBubble';
 
 class App extends React.Component {
 
@@ -41,11 +42,18 @@ class App extends React.Component {
         for(let i = 0; i < roomsData.length; i++) {
           const room = roomsData[i];
           const usersInRoom = await axios.get(`http://localhost:8080/api/rooms/${room.id}`);
+          const messagesInRoom = await axios.get(`http://localhost:8080/api/rooms/${room.id}/messages`);
+          console.log('messagesInRoom:');
+          console.dir(messagesInRoom);
+          let usersMessagesInRoom = {};
           if(usersInRoom && usersInRoom.data && usersInRoom.data.users) {
-            const aggregatedData = Object.assign({users: usersInRoom.data.users}, roomsData[i]);
-            enhancedRoomsData.push(aggregatedData);
-            
+            usersMessagesInRoom.users = usersInRoom.data.users;
           }
+          if(messagesInRoom && messagesInRoom.data && messagesInRoom.data) {
+            usersMessagesInRoom.messages = messagesInRoom.data;
+          }
+          const aggregatedData = Object.assign(usersMessagesInRoom, roomsData[i]);
+            enhancedRoomsData.push(aggregatedData);
           
         }
 
@@ -79,81 +87,6 @@ class App extends React.Component {
     });
 
   }
-
-  renderZZ() {
-    return (
-      <div className="ui sidebar inverted vertical menu visible">
-        {this.state.listOfRooms.map( room => (
-          <>
-          <div className={this.state.selectedRoom.name === room.name ? "item active" : "item"} key={room.id} onClick={() => this.handleClick(room.id)}>
-            <span><i className="users icon"></i></span>
-            <span style={{paddingLeft: "10px"}}>{room.name}</span>
-          </div>
-          {this.state.selectedRoom.name === room.name && (
-            <div className="ui link list">
-            { this.state.selectedRoom.listOfUsers.map( user => (
-              <div className="item" key={user} style={{paddingLeft: "30px"}}>
-                <span style={{marginRight: '15px', }}><img width="20px" height="20px" alt="avatar" src={faker.image.avatar()} /></span>
-                <span>{user}</span>
-              </div>
-            ))}
-            </div>
-          )}
-          </>
-        )) }
-      </div>
-    );
-  }
-
-  renderYY() {
-    return (
-      <>
-      <div className="ui two column grid">
-        <div className="column">
-          <div className="ui left sidebar inverted vertical menu visible" style={{padding: "15px", backgroundColor: "#000", color: "#fff"}}>
-            {this.state.listOfRooms.map( room => (
-              <>
-                <div className={this.state.selectedRoom.name === room.name ? "item active" : "item"} key={room.id} onClick={() => this.handleClick(room.id)}>
-                  <span><i className="users icon"></i></span>
-                  <span style={{paddingLeft: "10px"}}>{room.name}</span>
-                </div>
-                {this.state.selectedRoom.name === room.name && (
-                  <div className="ui middle aligned selection list" style={{paddingLeft: "15px"}}>
-                    {this.state.selectedRoom.listOfUsers.map( user => (
-                      <div 
-                        className={this.state.selectedUser === user || this.state.hoverUser === user ? "item active" : "item"}  
-                        onMouseEnter={() => this.setState({hoverUser: user})}
-                        onMouseLeave={() => this.setState({hoverUser: ""})}
-                        onClick={() => this.setState({selectedUser: user})}
-                      >
-                        <img className="ui avatar image" src={faker.image.avatar()} />
-                        <span className="content">
-                          {user}
-                        </span>
-                      </div>
-                    ))}
-                  </div>
-                )}
-              </>
-            ))}
-           
-          </div>
-        </div>
-        <div className="wide column">
-          <div className="ui fluid action input full">
-            <input type="text" placeholder="Type here..." />
-            <div className="ui button">Send</div>
-          </div>
-        </div>
-      </div>
-      </>
-    );
-  }
-
-  // async getFakerAvatar() {
-  //   const avatarUrl = await faker.image.avatar();
-  //   return avatarUrl;
-  // }
 
   getUserAvatar = (userName) => {
     let cachedUserAvatar = this.state.users[userName] || null;
@@ -196,8 +129,7 @@ class App extends React.Component {
               ))}
         </div>
         <div className="box content">
-          Content
-          <br /> More content than we had before so this column is now quite tall.
+          <ChatBubble />
         </div>
         <div className="box footer">
           <div className="ui fluid action input full">
---
+++ src/components/ChatBubble.js
@@ -0,0 +1,7 @@
+import React from 'react';
+
+const ChatBubble = (props) => {
+  return <div>I am a chat bubble</div>;
+};
+
+export default ChatBubble;
